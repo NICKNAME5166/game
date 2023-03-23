3ecb16c88b0003ec1dd3a8ec25b64b55d4a31602
Nikita Levchenko <nikita.levchenko.007@gmail.com>
2023-03-23 15:22:51 +0200

    centered everything

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ class Sprite {
     constructor({position}){
       this.position = position
       this.image = new Image
-      this.image.src = ''
-
+      this.image.src = './img/backgroundLevel1.png'
+    }
+    draw(){
+      c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
+const backgroundLevel1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  }
+})
 const player = new Player()
 
 const keys = {
@@ -26,8 +34,7 @@ const keys = {
 }
 function animate (){
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height)
+    backgroundLevel1.draw()
     
     player.velocity.x = 0
     if (keys.d.pressed) {
