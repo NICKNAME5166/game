47783e89485e0648676ec152ecba1d15ba43f235
sh4dm4nsyed <126772252+sh4dm4nsyed@users.noreply.github.com>
2023-03-23 10:21:00 +0100

    Add files via upload
    -gravity added
    -player movement added

--- script.js
+++ script.js
@@ -5,11 +5,64 @@ canvas.height = 576
 
 
 const player = new Player()
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false,
+  },
+}
 function animate (){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    
+    player.velocity.x = 0
+    if (keys.d.pressed) {
+      player.velocity.x = 1
+    } else if (key.a.pressed){
+      player.velocity.x = -1
+    }
     player.draw()
     player.update()
 }
+
 animate()
+
+
+window.addEventListener("keydown", (event) => {
+ console.log(event.key)
+ switch (event.key) {
+    case "w":
+      if (player.velocity.y === 0) player.velocity.y =  -20
+      
+    break
+    case "a":
+    // move player to the left
+    keys.a.pressed = true
+    break
+    case "d":
+    // move player to the right
+    keys.d.pressed = true
+    break
+ }
+})
+
+window.addEventListener("keyup", (event) => {
+    console.log(event.key)
+    switch (event.key) {
+       case "a":
+       // move player to the left
+       keys.a.pressed = false
+       break
+       case "d":
+       // move player to the right
+       keys.d.pressed = false 
+       break
+    }
+   })
