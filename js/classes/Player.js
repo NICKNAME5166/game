76979e8dcffc94e74aad14854ce100791f0bacf8
sh4dm4nsyed <126772252+sh4dm4nsyed@users.noreply.github.com>
2023-03-23 10:32:35 +0100

    Add files via upload

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,11 +4,18 @@ class Player {
             x: 100,
             y: 100,
         }
+
+        this.velocity = {
+            x:0,
+            y:0,
+        }
+
         this.width = 100
         this.height = 100
         this.sides = {
-            bottom: this.position.y + this.heigth,
+            bottom: this.position.y + this.height,
         }
+        this.gravity = 1
     }
     draw(){
         c.fillStyle = 'red'
@@ -16,12 +23,13 @@ class Player {
         this.update()
     }
     update(){
-        if(this.sides.bottom < canvas.height){
-        this.position.y++
-        this.sides.bottom = this.position.y + this.height
-        console.log('goeg');
-        }
-        console.log(this.position.y + this.heigth,);
+     this.position.x += this.velocity.x
+     this.position.y += this.velocity.y
+     this.sides.bottom = this.position.y + this.height
 
+     // above bottom of canvas
+     if(this.sides.bottom + this.velocity.y < canvas.height){
+        this.velocity.y += this.gravity
+        } else this.velocity.y = 0
     }
 }
