8f24272a244eaf12a201e60a44d50a700d7d0eba
Nikita Levchenko <nikita.levchenko.007@gmail.com>
2023-03-30 15:10:07 +0300

    30.03.23 First part of the detection

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -29,29 +29,53 @@ class Player {
     }
     update(){
      this.position.x += this.velocity.x
-    //  this.collisionBlocks.forEach()
+    //this.collisionBlocks.forEach()
      for (let i = 0; i < this.collisionBlocks.length; i++) {
-        const collisionBlocks = this.collisionBlocks[i]
+        const collisionBlock = this.collisionBlocks[i]
 
-        if (this.position.x <= collisionBlocks.position.x + collisionBlocks.width &&
-            this.position.x + this.width >= collisionBlocks.position.x &&
-            this.position.y + this.height >= collisionBlocks.position.x &&
-            this.position.y <= collisionBlocks.position.y + collisionBlocks.height
+        if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+            this.position.x + this.width >= collisionBlock.position.x &&
+            this.position.y + this.height >= collisionBlock.position.y &&
+            this.position.y <= collisionBlock.position.y + collisionBlock.height
         ) {
           if (this.velocity.x < -1){
             this.position.x =
-             collisionBlocks.position.x + collisionBlocks.width + 0.01
+             collisionBlock.position.x + collisionBlock.width + 0.01
              break
           }
 
           if (this.velocity.x > 1){
              this.position.x = collisionBlock.position.x - this.width -0.01
-
+             break
           }
         }
      }
+
+
      this.position.y += this.velocity.y
      this.sides.bottom = this.position.y + this.height
+
+     for (let i = 0; i < this.collisionBlocks.length; i++) {
+        const collisionBlock = this.collisionBlocks[i]
+
+        if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+            this.position.x + this.width >= collisionBlock.position.x &&
+            this.position.y + this.height >= collisionBlock.position.y &&
+            this.position.y <= collisionBlock.position.y + collisionBlock.height
+        ) {
+          if (this.velocity.y < -1){
+            this.position.y =
+             collisionBlock.position.y + collisionBlock.height + 0.01
+             break
+          }
+
+          if (this.velocity.y > 1){
+             this.position.y = collisionBlock.position.y - this.height - 0.01
+             break
+          }
+        }
+     }
+
 
      if(this.sides.bottom + this.velocity.y < canvas.height){
         this.velocity.y += this.gravity
