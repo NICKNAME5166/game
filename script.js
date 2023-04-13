10fbe11c39852d5e2faf77fd5bfb35676d78e77a
sh4dm4nsyed <126772252+sh4dm4nsyed@users.noreply.github.com>
2023-04-13 11:47:50 +0200

    Add files via upload
    can you fix please the bug of hitbox implementation? I`ve almost done with that. please fix this at the vacation, if you can.

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-const parsedCollisions = collisionsLevel1.parse2D()
-const collisionBlocks = parsedCollisions.createObjects2D()
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjects2D();
 
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
     y: 0,
   },
-  imgSrc: './img/backgroundLevel1.png',
-})
-
+  imageSrc: './img/backgroundLevel1.png',
+});
 
 const player = new Player({
   collisionBlocks, 
-})
+  imageSrc: './img/king/idle.png',
+  frameRate: 11,
+}); //Das ist Shadman sein Teil
 
-
-function animate (){
-    window.requestAnimationFrame(animate)
-    backgroundLevel1.draw()
-    collisionBlocks.forEach((CollisionBlock) => {
-      CollisionBlock.draw()
-    })
-    player.velocity.x = 0
-    if (keys.d.pressed) {
-      player.velocity.x = 5
-    } else if (keys.a.pressed){
-      player.velocity.x = -5
-    }
-    player.update()
-    player.draw()
-    
-    
+function animate() {
+  window.requestAnimationFrame(animate);
+  backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.a.pressed){
+    player.velocity.x = -5;
+  }
+  player.update();
+  player.draw();
 }
 
-animate()
+animate(); //Das ist Shadman sein Teil
